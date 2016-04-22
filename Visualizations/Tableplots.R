f60735fa44fcde3154a168a94a22d251182870c3
Laurae2 <Laurae2@users.noreply.github.com>
2016-04-22 23:11:27 +0200

    Update Tableplots.R

--- Visualizations/Tableplots.R
+++ Visualizations/Tableplots.R
@@ -4,9 +4,9 @@
 remove(list = ls())
 setwd("C:/Users/Laurae/Documents/Data Science/Santander/")
 train <- read.csv("train.csv")
-a <- tablePrepare(data.frame(target = as.factor(train$TARGET), train[, -371]))
 
 library(tableplot)
+a <- tablePrepare(data.frame(target = as.factor(train$TARGET), train[, -371]))
 for (i in 2:371) {
   jpeg(filename = paste("./autoplots/plot_", i, ".jpg", sep = ""), width = 960, height = 960, units = "px", pointsize = 12)
   tableplot(dat = a, select = c(1,i), sortCol = target, sample = FALSE, nBins = 101)
