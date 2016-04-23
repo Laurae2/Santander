e1e102062c06fa1841dc82706db65e8f908f454a
Laurae2 <Laurae2@users.noreply.github.com>
2016-04-24 00:18:28 +0200

    Update Duplicates Removal.R

--- Outliers/Duplicates Removal.R
+++ Outliers/Duplicates Removal.R
@@ -55,7 +55,7 @@ for (i in temp_i) {
       }
       #setTxtProgressBar(prog_bar, j/NROW(temp_j))
     }
-    tempScript <- paste(tempScript, ifelse(temp_negatives/24 < (temp_positives+1), paste(tempList2, ", ", tempList1, sep = ""), paste(tempList1, ", ", i, sep = "")), "), ]\n", sep = "")
+    tempScript <- paste(tempScript, ifelse(temp_negatives/24 < (temp_positives+1), paste(tempList2, paste(tempList2, ifelse(tempList2 == "", "", ", ", sep = ""), tempList1, sep = ""), paste(tempList1, ", ", i, sep = "")), "), ]\n", sep = "")
     cat(tempScript, sep = "")
     #close(prog_bar)
   }
