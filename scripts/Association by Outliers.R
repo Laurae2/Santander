9d96102537111c0e965c8dc518dd98abc1f3fc71
Laurae2 <Laurae2@users.noreply.github.com>
2016-04-26 17:12:32 +0200

    Updated Script to get 0.842534 (top 30)

--- scripts/Association by Outliers.R
+++ scripts/Association by Outliers.R
@@ -27,6 +27,44 @@ train_target <- train$TARGET
 train_temp$TARGET <- NULL
 
 
+##### 0 count per line
+count0 <- function(x) {
+  return( sum(x == 0) )
+}
+train_temp$n0 <- apply(train_temp, 1, FUN=count0)
+test_temp$n0 <- apply(test_temp, 1, FUN=count0)
+
+##### Removing constant features
+cat("\n## Removing the constants features.\n")
+for (f in names(train_temp)) {
+  if (length(unique(train_temp[[f]])) == 1) {
+    cat(f, "is constant in train. We delete it.\n")
+    train_temp[[f]] <- NULL
+    test_temp[[f]] <- NULL
+  }
+}
+
+##### Removing identical features
+features_pair <- combn(names(train_temp), 2, simplify = F)
+toRemove <- c()
+for(pair in features_pair) {
+  f1 <- pair[1]
+  f2 <- pair[2]
+  
+  if (!(f1 %in% toRemove) & !(f2 %in% toRemove)) {
+    if (all(train_temp[[f1]] == train_temp[[f2]])) {
+      cat(f1, "and", f2, "are equals.\n")
+      toRemove <- c(toRemove, f2)
+    }
+  }
+}
+
+feature.names <- setdiff(names(train_temp), toRemove)
+
+train_temp <- train_temp[, feature.names]
+test_temp <- test_temp[, feature.names]
+
+
 # ~~~~ GRADIENT DESCENT ASSOCIATION RULE TESTING
 optimized_func <- function(target, scores, min_score, min_node, false_neg, cutoff) {
   # cutoff has two values: the minimum and maximum. Everything between is dished out.
