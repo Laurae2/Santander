f8f38b8e7322f8923b2a4a6cfbd6bcaaadff8e94
Laurae2 <Laurae2@users.noreply.github.com>
2016-04-28 18:53:30 +0200

    Added basic R script

--- scripts/Repeated CV.R
+++ scripts/Repeated CV.R
@@ -2,8 +2,62 @@ library(caret)
 library(R.utils)
 
 setwd("C:/Users/Laurae/Documents/Data Science/Santander/") ##set your own working directory
-train_temp <- train ##set train_temp to the appropriate training set
-train_target <- target ##set train_target to the label
+#train_temp <- train ##set train_temp to the appropriate training set
+#train_target <- target ##set train_target to the label
+
+train <- read.csv("train.csv")
+test  <- read.csv("test.csv")
+
+train_temp <- train
+test_temp <- test
+train_target <- train$TARGET
+
+##### Removing IDs
+train_temp$ID <- NULL
+test.id <- test_temp$ID
+test_temp$ID <- NULL
+
+##### Extracting TARGET
+train.y <- train_temp$TARGET
+train_temp$TARGET <- NULL
+
+##### 0 count per line
+count0 <- function(x) {
+  return( sum(x == 0) )
+}
+train_temp$n0 <- apply(train_temp, 1, FUN=count0)
+test_temp$n0 <- apply(test_temp, 1, FUN=count0)
+
+##### Removing constant features
+cat("\n## Removing the constants features.\n")
+for (f in names(train_temp)) {
+  if (length(unique(train_temp[[f]])) == 1) {
+    cat(f, "is constant in train. We delete it.\n")
+    train_temp[[f]] <- NULL
+    test_temp[[f]] <- NULL
+  }
+}
+
+##### Removing identical features
+features_pair <- combn(names(train_temp), 2, simplify = F)
+toRemove <- c()
+for(pair in features_pair) {
+  f1 <- pair[1]
+  f2 <- pair[2]
+  
+  if (!(f1 %in% toRemove) & !(f2 %in% toRemove)) {
+    if (all(train_temp[[f1]] == train_temp[[f2]])) {
+      cat(f1, "and", f2, "are equals.\n")
+      toRemove <- c(toRemove, f2)
+    }
+  }
+}
+
+feature.names <- setdiff(names(train_temp), toRemove)
+
+train_temp <- train_temp[, feature.names]
+test_temp <- test_temp[, feature.names]
+
 set.seed(11111)
 folds <- createMultiFolds(train_target, k = 4, times = 3)
 
