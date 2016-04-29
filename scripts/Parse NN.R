10b5b1b73c80af186f7a89776497a16a02ac4151
Laurae2 <Laurae2@users.noreply.github.com>
2016-04-29 18:50:15 +0200

    Combine original set with parsed set

--- scripts/Parse NN.R
+++ scripts/Parse NN.R
@@ -22,3 +22,57 @@ full_data_temp <- full_data[-151839, !(colnames(full_data) %in% c("SinglesNN15",
 
 
 #findCorrelation(cor(full_data_temp), cutoff = .999, verbose = TRUE, names = TRUE)
+
+train <- read.csv("train.csv")
+test  <- read.csv("test.csv")
+
+
+train_temp <- train
+test_temp <- test
+
+# removing ID
+train_temp$ID <- NULL
+test_temp$ID <- NULL
+
+# extracting label
+train_target <- train$TARGET
+train_temp$TARGET <- NULL
+
+# 0 count per line
+count0 <- function(x) {
+  return( sum(x == 0) )
+}
+train_temp$n0 <- apply(train_temp, 1, FUN=count0)
+test_temp$n0 <- apply(test_temp, 1, FUN=count0)
+
+# Removing constant features
+cat("\n## Removing the constants features.\n")
+for (f in names(train_temp)) {
+  if (length(unique(train_temp[[f]])) == 1) {
+    cat(f, "is constant.\n")
+    train_temp[[f]] <- NULL
+    test_temp[[f]] <- NULL
+  }
+}
+
+# Removing identical features
+features_pair <- combn(names(train_temp), 2, simplify = F)
+toRemove <- c()
+for(pair in features_pair) {
+  f1 <- pair[1]
+  f2 <- pair[2]
+  
+  if (!(f1 %in% toRemove) & !(f2 %in% toRemove)) {
+    if (all(train_temp[[f1]] == train_temp[[f2]])) {
+      cat(f1, "and", f2, "are equal.\n")
+      toRemove <- c(toRemove, f2)
+    }
+  }
+}
+
+feature.names <- setdiff(names(train_temp), toRemove)
+
+train_temp <- train_temp[, feature.names]
+test_temp <- test_temp[, feature.names]
+
+full_data_temp <- cbind(full_data_temp, rbind(train_temp, test_temp))
