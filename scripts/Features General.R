4e6b01142cc8d0845d0974b4ca9d0c6e974335d0
Laurae2 <Laurae2@users.noreply.github.com>
2016-05-01 14:07:30 +0200

    4 sets of data now

--- scripts/Features General.R
+++ scripts/Features General.R
@@ -57,7 +57,7 @@ feature.names <- setdiff(names(train_temp), toRemove)
 train_temp <- train_temp[, feature.names]
 test_temp <- test_temp[, feature.names]
 
-train_temp$TARGET <- train_target
+#train_temp$TARGET <- train_target
 
 write.csv(train_temp, file = "train_set1.csv", row.names = FALSE)
 write.csv(test_temp, file = "test_set1.csv", row.names = FALSE)
@@ -92,7 +92,7 @@ feature.names <- c("var15", "saldo_var30", "n0", "num_var22_ult3", "imp_op_var39
 train_temp <- train_temp[, feature.names]
 test_temp <- test_temp[, feature.names]
 
-train_temp$TARGET <- train_target
+#train_temp$TARGET <- train_target
 
 write.csv(train_temp, file = "train_set2.csv", row.names = FALSE)
 write.csv(test_temp, file = "test_set2.csv", row.names = FALSE)
@@ -187,7 +187,36 @@ feature.names <- c('num_var39_0',  # 0.00031104199066874026
 train_temp <- train_temp[, feature.names]
 test_temp <- test_temp[, feature.names]
 
-train_temp$TARGET <- train_target
+#train_temp$TARGET <- train_target
 
 write.csv(train_temp, file = "train_set3.csv", row.names = FALSE)
 write.csv(test_temp, file = "test_set3.csv", row.names = FALSE)
+
+
+
+# Generate Set 4
+
+setwd("C:/Users/Laurae/Documents/Data Science/Santander/") ##set your own working directory
+full_data <- as.data.frame(fread("fulldata - Copy2.csv", header = TRUE, sep = ","))
+full_data_temp <- full_data[-151839, !(colnames(full_data) %in% c("SinglesNN15", "SinglesNN30", "SinglesNN38",
+                                                                  "NzvNN15", "NzvNN30", "NzvNN38",
+                                                                  "var5NN15", "var5NN30", "var5NN38",
+                                                                  "var8NN15", "var8NN30", "var8NN38",
+                                                                  "var12NN15", "var12NN30", "var12NN38",
+                                                                  "var13NN15", "var13NN30", "var13NN38",
+                                                                  "var30NN15", "var30NN30", "var30NN38",
+                                                                  "var37NN15", "var37NN30", "var37NN38",
+                                                                  "var39NN15", "var39NN30", "var39NN38",
+                                                                  "var41NN15", "var41NN30", "var41NN38",
+                                                                  "var42NN15", "var42NN30", "var42NN38",
+                                                                  "var43NN15", "var43NN30", "var43NN38",
+                                                                  "var45NN15", "var45NN30", "var45NN38"))]
+
+train_temp <- full_data_temp[1:76020, ]
+test_temp <- full_data_temp[76021:151838, ]
+
+write.csv(train_temp, file = "train_set4.csv", row.names = FALSE)
+write.csv(test_temp, file = "test_set4.csv", row.names = FALSE)
+
+write.csv(train_target, file = "train_target.csv", row.names = FALSE, col.names = FALSE)
+
